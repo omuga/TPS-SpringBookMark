c3a3fd5ab4ee9837838da66962b2b213b3582307
omuga <Obriel.muga.14@sansano.usm.cl>
2019-04-29 12:37:39 -0400

    Agregar Dominio User

---
+++ UserService/user/src/main/java/src/models/User.java
@@ -0,0 +1,18 @@
+package src.models;
+
+public class User {
+    private int id;
+
+    public User(int id){
+        this.id = id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+    
+}
